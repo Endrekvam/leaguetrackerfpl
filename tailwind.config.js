d1ec18e8a56af80fcf6166081e3564e7d260ebdf
johannes <leite.johannes@gmail.com>
2024-11-02 18:58:01 +0100

    convert to vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-// tailwind.config.js
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
-
+}
